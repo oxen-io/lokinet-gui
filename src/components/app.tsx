d9a06b2ad230602bd8bb3f0d50dd902cad8ffa84
Audric Ackermann <audric@loki.network>
2021-02-18 16:37:04 +1100

    add some UI components from Chakra

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,19 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 
-import '../style/reset.css';
-import 'semantic-ui-css/semantic.min.css';
-import '../style/global.css';
-
 import {
-  Button,
-  Checkbox,
-  Divider,
-  Grid,
-  GridRow,
-  Header
-} from 'semantic-ui-react';
+  IconButton,
+  Heading,
+  ChakraProvider,
+  Switch,
+  Center,
+  StatLabel,
+  Stat,
+  StatNumber,
+  StatHelpText,
+  Box,
+  Flex,
+  HStack,
+  Icon,
+  Text,
+  Stack,
+  useInterval
+} from '@chakra-ui/react';
+
+import { VictoryArea, VictoryGroup, VictoryChart, VictoryTheme } from 'victory';
+
+import { MdPowerSettingsNew } from 'react-icons/md';
+
+import { FiUploadCloud, FiDownloadCloud } from 'react-icons/fi';
+
 import { getStatus, initializeIpcRendererSide } from '../ipc/ipc_renderer';
+import _ from 'lodash';
 
 const mainElement = document.createElement('div');
 document.body.appendChild(mainElement);
@@ -21,44 +36,139 @@ document.body.appendChild(mainElement);
 initializeIpcRendererSide();
 
 const EnableExitToggle = () => {
-  return <Checkbox slider />;
+  return <Switch colorScheme="whiteAlpha" size="lg" margin="auto" />;
 };
 
 const StopAndStart = () => {
   const [clicked, setClicked] = useState(false);
   return (
-    <Button
-      circular
-      inverted
-      // disabled={clicked}
-      loading={clicked}
+    <IconButton
+      margin="auto"
+      isRound
+      isLoading={clicked}
       onClick={() => {
-        setClicked(false);
+        setClicked(true);
         getStatus().then(console.warn);
       }}
-      size="massive"
-      icon="power off"
+      size="lg"
+      icon={<MdPowerSettingsNew />}
+      aria-label="stop and start"
     />
+  );
+};
+
+const StatsBox = ({ children }: { children: React.ReactNode }) => (
+  <Box
+    maxW="sm"
+    borderWidth="1px"
+    borderRadius="lg"
+    overflow="hidden"
+    shadow="xs"
+  >
+    {children}
+  </Box>
+);
+
+const ActivePathStats = () => (
+  <StatsBox>
+    <Stat padding="5px">
+      <StatLabel>Active Paths</StatLabel>
+      <StatNumber>3</StatNumber>
+      <StatHelpText>(50% success)</StatHelpText>
+    </Stat>
+  </StatsBox>
+);
+
+const LokinetRoutersStats = () => (
+  <StatsBox>
+    <Stat padding="5px">
+      <StatLabel>Lokinet Routers</StatLabel>
+      <StatNumber>1294</StatNumber>
+    </Stat>
+  </StatsBox>
+);
+
+const UpSpeedStats = () => (
+  <Box>
+    <HStack>
+      <Icon margin="5px" as={FiUploadCloud} />
+      <Text>7.44 KB/s</Text>
+    </HStack>
+  </Box>
+);
+
+const DownSpeedStats = () => (
+  <Box>
+    <HStack>
+      <Icon margin="5px" as={FiDownloadCloud} />
+      <Text>5.33 KB/s</Text>
+    </HStack>
+  </Box>
+);
+
+function getData(): Array<Array<{ x: number; y: number }>> {
+  return _.range(2).map(() => {
+    return [
+      { x: 1, y: _.random(1, 5) },
+      { x: 2, y: _.random(1, 10) },
+      { x: 3, y: _.random(2, 10) },
+      { x: 4, y: _.random(2, 10) },
+      { x: 5, y: _.random(2, 15) }
+    ];
+  });
+}
+
+const SpeedChart = () => {
+  const [data, setData] = useState([[]]);
+
+  useInterval(() => {
+    setData(getData() as any);
+  }, 4000);
+
+  return (
+    <VictoryChart
+      animate={{
+        duration: 400
+      }}
+      height={200}
+      theme={VictoryTheme.material}
+    >
+      <VictoryGroup
+        colorScale="grayscale"
+        style={{
+          data: { fillOpacity: 0.4 }
+        }}
+      >
+        {data.map((data, i) => {
+          return <VictoryArea key={i} data={data} interpolation="basis" />;
+        })}
+      </VictoryGroup>
+    </VictoryChart>
   );
 };
 
 const App = () => {
   return (
-    <Grid centered padded>
-      <GridRow>
-        <Header inverted size="large">
-          Lokinet
-        </Header>
-      </GridRow>
-      <GridRow>
-        <StopAndStart />
-      </GridRow>
-      <GridRow>
-        <EnableExitToggle />
-      </GridRow>
-
-      <Divider />
-    </Grid>
+    <ChakraProvider resetCSS={true}>
+      <Center height="100vh">
+        <Stack padding="20px">
+          <Heading size="4xl">Lokinet</Heading>
+          <Flex>
+            <StopAndStart />
+          </Flex>
+          <Flex>
+            <EnableExitToggle />
+          </Flex>
+          <ActivePathStats />
+          <LokinetRoutersStats />
+          <HStack justify="space-between">
+            <UpSpeedStats />
+            <DownSpeedStats />
+          </HStack>
+          <SpeedChart />
+        </Stack>
+      </Center>
+    </ChakraProvider>
   );
 };
 
