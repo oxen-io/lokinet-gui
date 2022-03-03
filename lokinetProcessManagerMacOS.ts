28f2c971171ed426790362c0a0553fb0b8232e56
audric <audric@Alexs-Mac-mini.local>
2022-03-03 14:56:45 +1100

    fix display notification mock call for start/stop macos

--- lokinetProcessManagerMacOS.ts
+++ lokinetProcessManagerMacOS.ts
@@ -3,13 +3,13 @@ import { ILokinetProcessManager, invoke } from './lokinetProcessManager';
 export class LokinetMacOSProcessManager implements ILokinetProcessManager {
   doStartLokinetProcess(): Promise<string | null> {
     const startNotification =
-      '-e \'display notification\' "We should start LOKINET mac extension"';
+      '-e \'display notification "We should start LOKINET mac extension"\'';
     return invoke('osascript', [startNotification]);
   }
 
   doStopLokinetProcess(): Promise<string | null> {
     const stopNotification =
-      '-e \'display notification\' "We should stop LOKINET mac extension"';
+      '-e \'display notification "We should stop LOKINET mac extension"\'';
 
     return invoke('osascript', [stopNotification]);
   }
